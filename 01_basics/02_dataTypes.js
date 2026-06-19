b4062b5912ce3f472c351222e2500d33768f63cd
Ekshith <ekshithkumarmamidisetti@gmail.com>
2026-06-19 20:36:49 +0530

    DataTypes In Js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" // treats all js code as newer js version
+let name="Ekshith"
+let age=20
+let isLoggedIn=true;
+
+// Primitive DataTypes 
+//  number
+//  bigint
+//  String
+// boolean 
+// null 
+// undefined
+// symbol 
+
+console.log(typeof 12);
+console.log(typeof "Ekshith");
+console.log(typeof Ekshith);
