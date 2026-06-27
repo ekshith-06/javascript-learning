1be0ee59faac989696690f88e2105d9299631ced
Ekshith <ekshithkumarmamidisetti@gmail.com>
2026-06-27 18:52:44 +0530

    String Interpolation Js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name="Ekshith "
+let val=39;
+// Concatenation
+console.log(name+ val+ " Hello");
+
+// Interpolation
+// Instead of concatenation using placeholders so that its easy to write 
+
+console.log(`Hello my name is ${name}and my number is ${val}`);
+console.log(`Hello my name is ${name.toUpperCase()}and my number is ${val}`);
+
+let user=new String("Ekshith")
+
+console.log(user.charAt(0));
+console.log(user.indexOf('h'));
+
+console.log(user.toUpperCase());
+
+
+
